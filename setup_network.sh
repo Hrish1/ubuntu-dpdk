f3ea09952c44a1f439bd362421b844d09a66cccf
Cody Doucette <doucette@bu.edu>
2018-06-21 00:02:17 -0400

    Configure IP addresses for host-only networks

--- setup_network.sh
+++ setup_network.sh
@@ -4,13 +4,17 @@
 
 if ! ls /sys/class/net | grep -q "vboxnet0"; then
     VBoxManage hostonlyif create
+    VBoxManage hostonlyif ipconfig vboxnet0 -ip 10.0.10.254
 fi
 if ! ls /sys/class/net | grep -q "vboxnet1"; then
     VBoxManage hostonlyif create
+    VBoxManage hostonlyif ipconfig vboxnet1 -ip 10.0.11.254
 fi
 if ! ls /sys/class/net | grep -q "vboxnet2"; then
     VBoxManage hostonlyif create
+    VBoxManage hostonlyif ipconfig vboxnet2 -ip 10.0.12.254
 fi
 if ! ls /sys/class/net | grep -q "vboxnet3"; then
     VBoxManage hostonlyif create
+    VBoxManage hostonlyif ipconfig vboxnet3 -ip 10.0.13.254
 fi
